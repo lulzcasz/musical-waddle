69bc20e061d67a85e4a6adb1572a7de86d81ae93
lulzcasz <reisdossousalucasedson@gmail.com>
2024-06-27 18:25:34 -0300

    feat: create configuration file

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    app.config['DEBUG'] = True
+    app.config.from_pyfile('config.py')
 
     from .blueprints import content as content_bp
     app.register_blueprint(content_bp)
