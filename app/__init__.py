4d2c1ddf222fddd5064d29574e5d5a0a0f91c5df
lulzcasz <reisdossousalucasedson@gmail.com>
2024-07-02 21:58:06 -0300

    feat: use Flask CLI instead run.py entrypoint

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    app.config['DEBUG'] = True
-
     from .blueprints import content as content_bp
     app.register_blueprint(content_bp)
 
